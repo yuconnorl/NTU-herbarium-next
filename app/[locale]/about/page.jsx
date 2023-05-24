040889cdd0125db4773b435eb21937eae17f70e8
yuconnorl <r06b44002@ntu.edu.tw>
2023-05-24 15:31:44 +0800

    feat: all pages

--- app/[locale]/about/page.jsx
+++ app/[locale]/about/page.jsx
@@ -6,16 +6,16 @@ import imgFrontDoorWebp from '@/assets/images/about/frontDoor.webp'
 import { DecoHeading, Heading, Paragraph } from '@/components/Letters'
 
 const AboutPage = () => {
-  const t = useTranslations('Landing')
+  const t = useTranslations('About')
 
   return (
     <>
       <div className='pl-8 md:pl-16'>
         <div className='mb-10 pt-24'>
-          <DecoHeading>{t('about_miniTitle')}</DecoHeading>
-          <Heading className='mb-6 md:mb-10'>{t('about_title')}</Heading>
+          <DecoHeading>{t('miniTitle')}</DecoHeading>
+          <Heading className='mb-6 md:mb-10'>{t('title')}</Heading>
           <Paragraph className='w-4/5 leading-relaxed tracking-wider text-onyx md:w-4/6 md:text-lg md:leading-loose md:tracking-widest'>
-            {t('about_intro')}
+            {t('intro')}
           </Paragraph>
         </div>
         <Image src={imgFrontDoorWebp} alt='front door of NTU herbarium' />
--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -23,20 +23,20 @@ const Hero = () => (
 )
 
 const Collection = () => {
-  const t = useTranslations('Index')
+  const t = useTranslations('Landing')
 
   return (
     <section className='relative px-10 pt-6 pb-8 sm:px-16 mdp:px-28 lg:pb-24 lg:pt-20'>
       <div className='mb-20 mdp:mb-12'>
-        <DecoHeading>{t('landing_collection_miniTitle')}</DecoHeading>
+        <DecoHeading>{t('collection_miniTitle')}</DecoHeading>
         <Heading className='mb-10 md:max-w-1/2 mdp:mb-16 text-4xl md:text-[3rem] md:leading-tight'>
-          {t('landing_collection_title')}
+          {t('collection_title')}
         </Heading>
         <Paragraph className='mdp:w-2/5 md:w-3/6 md:text-lg'>
-          {t('landing_collection_subtitle')}
+          {t('collection_subtitle')}
         </Paragraph>
       </div>
-      <LinkButton url='collection' text={t('landing_collection_button')} />
+      <LinkButton url='collection' text={t('collection_button')} />
       <TextSpinner text='NTU Herbarium * NTU Herbarium *' />
     </section>
   )
@@ -52,17 +52,15 @@ const News = () => (
 )
 
 const Exhibition = () => {
-  const t = useTranslations('Index')
+  const t = useTranslations('Landing')
   return (
     <section className='relative mt-4 flex w-full flex-col pb-16 sm:px-8 md:mt-16 md:px-12 mdp:flex-row'>
       <div className='px-10 mdp:sticky mdp:top-6 mdp:mb-80 mdp:h-content mdp:w-4/12'>
-        <DecoHeading>{t('landing_exhibition_miniTitle')}</DecoHeading>
+        <DecoHeading>{t('exhibition_miniTitle')}</DecoHeading>
         <Heading className='mb-6 md:mb-10 text-3xl leading-tight md:text-4xl md:leading-tight'>
-          {t('landing_exhibition_title')}
+          {t('exhibition_title')}
         </Heading>
-        <Paragraph className='lg:w-10/12 text-lg'>
-          {t('landing_exhibition_subtitle')}
-        </Paragraph>
+        <Paragraph className='lg:w-10/12 text-lg'>{t('exhibition_subtitle')}</Paragraph>
       </div>
       <div className='border-brown border-opacity-20 px-6 md:px-10 mdp:w-8/12 mdp:border-l mdp:pt-10'>
         <div className='grid-row-4 grid gap-x-16 gap-y-3 text-sm'>
@@ -72,16 +70,16 @@ const Exhibition = () => {
               <Image className='opacity-80' src={item.imgURL} alt={item.alt} />
               <div className='mt-4 pl-1'>
                 <Heading className='text-xl mb-3'>
-                  {t(`landing_exhibition_title_${item.key}`)}
+                  {t(`exhibition_title_${item.key}`)}
                 </Heading>
                 <Paragraph className='pr-3 md:pr-28'>
-                  {t(`landing_exhibition_description_${item.key}`)}
+                  {t(`exhibition_description_${item.key}`)}
                 </Paragraph>
               </div>
             </Fragment>
           ))}
         </div>
-        <LinkButton url='exhibition' text={t('landing_exhibition_button')} />
+        <LinkButton url='exhibition' text={t('exhibition_button')} />
       </div>
     </section>
   )
